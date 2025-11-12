f26808819fe86c794a37e22e37ddeb5f4db91688
willtrigo <willtrigo@gmail.com>
2025-11-12 12:53:17 -0300

    Feat: implement parseArguments  Changes to be committed: 	modified:   Makefile 	modified:   src/presentation/cli/CliController.cpp 	modified:   src/presentation/cli/CliController.hpp

--- ex02/src/presentation/cli/CliController.cpp
+++ ex02/src/presentation/cli/CliController.cpp
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 18:42:44 by dande-je          #+#    #+#             */
-/*   Updated: 2025/11/11 20:44:15 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/11/12 12:38:37 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 #include "presentation/cli/CliController.hpp"
 #include "presentation/cli/CliView.hpp"
 
+#include <algorithm>
+#include <cstdlib>
+#include <limits>
+#include <sstream>
 #include <stdexcept>
 #include <string>
+#include <vector>
 
 CliController::CliController(CliView& view) : m_view(view) {}
 
@@ -34,8 +39,7 @@ CliController& CliController::operator=(const CliController& other) {
 bool CliController::run(int argc, char** argv) {
   IntegerSequence sequence;
 
-  if (argc != MAX_SIZE_ARGS) {
-    this->m_view.displayUsage(std::string(argv[NAME_PROGRAM]));
+  if (!parseArguments(argc, argv, sequence)) {
     return false;
   }
 
@@ -45,4 +49,58 @@ bool CliController::run(int argc, char** argv) {
     return false;
   }
   return true;
+}
+
+bool CliController::parseArguments(int argc, char** argv,
+                                   IntegerSequence& sequence) {
+  if (argc < MAX_SIZE_ARGS) {
+    this->m_view.displayUsage(std::string(argv[NAME_PROGRAM]));
+    return false;
+  }
+
+  for (int i = LITERAL_ARGUMENT_INDEX; i < argc; ++i) {
+    std::istringstream iss(argv[i]);
+    long value;
+
+    if (!(iss >> value) || !iss.eof()) {
+      this->m_view.displayError("invalid input");
+      return false;
+    }
+
+    if (value < 0) {
+      this->m_view.displayError("negative value not allowed");
+      return false;
+    }
+
+    if (value > std::numeric_limits<int>::max()) {
+      this->m_view.displayError("value too large");
+      return false;
+    }
+
+    sequence.add(static_cast<int>(value));
+
+    if (hasDuplicates(sequence.getData())) {
+      this->m_view.displayError("duplicate values detected");
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool CliController::hasDuplicates(const std::vector<int>& data) {
+  if (data.size() <= 1) {
+    return false;
+  }
+
+  std::vector<int> sorted(data);
+  std::sort(sorted.begin(), sorted.end());
+
+  for (size_t i = 1; i < sorted.size(); ++i) {
+    if (sorted.at(i) == sorted.at(i - 1)) {
+      return true;
+    }
+  }
+
+  return false;
 }
--- ex02/src/presentation/cli/CliController.hpp
+++ ex02/src/presentation/cli/CliController.hpp
@@ -6,13 +6,14 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/10 18:43:09 by dande-je          #+#    #+#             */
-/*   Updated: 2025/11/10 18:43:16 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/11/12 12:36:59 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CLI_CONTRLLER_HPP
 #define CLI_CONTRLLER_HPP
 
+#include "domain/entities/IntegerSequence.hpp"
 #include "presentation/cli/CliView.hpp"
 
 class CliController {
@@ -32,6 +33,9 @@ class CliController {
   static const int MAX_SIZE_ARGS = 2;
   static const int NAME_PROGRAM = 0;
   static const int LITERAL_ARGUMENT_INDEX = 1;
+
+  bool parseArguments(int argc, char** argv, IntegerSequence& sequence);
+  static bool hasDuplicates(const std::vector<int>& data);
 };
 
 #endif  // CLI_CONTRLLER_HPP
